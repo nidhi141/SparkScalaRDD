fff1a5f01603cd5527adfa84da82731bbbecdf9f
nidhi141 <gpa.166170307109@gmail.com>
2023-05-18 21:36:21 +0530

    added new program

---
+++ src/main/scala/GTFS.scala
@@ -0,0 +1,45 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object GTFS extends App {
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  val spark = SparkSession.builder().appName("MyApp").master("local[*]").getOrCreate()
+
+  //Routes DataFrame
+  val routePath = "D:/Scala/gtfs_stm/routes.txt"
+  val routeDF: DataFrame= spark.read.option("header", "true")
+    .option("inferSchema", "true").csv(routePath)
+  println(routeDF.count())
+  routeDF.show()
+
+  //Trips DataFrame
+  val tripPath = "D:/Scala/gtfs_stm/trips.txt"
+  val tripDF: DataFrame = spark.read.option("header", "true")
+    .option("inferSchema", "true").csv(tripPath)
+  println(tripDF.count())
+  tripDF.show()
+
+  //Join Routes and Trips
+  val routeTripDF: DataFrame = routeDF.join(tripDF,routeDF("route_id") === tripDF("route_id"))
+  val joinedDFWithoutDuplicate : DataFrame= routeTripDF.drop(tripDF("route_id"))
+  println(joinedDFWithoutDuplicate.count())
+  joinedDFWithoutDuplicate.show()
+
+  //Calendar DataFrame
+  val calendarPath = "D:/Scala/gtfs_stm/calendar.txt"
+  val calendarDF: DataFrame = spark.read.option("header", "true")
+    .option("inferSchema", "true").csv(calendarPath)
+  println(calendarDF.count())
+  calendarDF.show()
+
+  //Join RouteTrip and Calendar
+  val routeTripCalendarDF: DataFrame = joinedDFWithoutDuplicate.join(calendarDF,
+      joinedDFWithoutDuplicate("service_id") === calendarDF("service_id"))
+  val joinedRTCDFWithoutDuplicate: DataFrame = routeTripCalendarDF.drop(tripDF("service_id"))
+  println(joinedRTCDFWithoutDuplicate.count())
+  joinedRTCDFWithoutDuplicate.show()
+
+  spark.stop()
+}
